b9d6001240696dc0478060ddeaa993ea3ab06989
Philipp Erhardt <philipperhardt29@gmail.com>
2020-06-02 11:03:53 +0200

    Improve mobile menu to work correctly all the time

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,10 +1,28 @@
 // https://stackoverflow.com/a/44897454
-(function() {
+var lastClick = 0;
+
+function openCloseMenu() {
+    // since this event might be called twice (once because of click, once because touchstart),
+    // we need to make sure it only fires once, else the menu would not be visible at all 
+    if (new Date() - lastClick < 200) {
+        return;
+    }
+    lastClick = new Date();
+
     var burger = document.querySelector('.navbar-burger');
     var menu = document.querySelector('.navbar-menu');
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
 
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+}
+
+function registerMenu() {
+    var burger = document.querySelector('.navbar-burger');
+
+    // click doesn't work in some mobile versions of chrome
+    // this is why we need to use touchstart too
+    burger.addEventListener('click', openCloseMenu);
+    burger.addEventListener('touchstart', openCloseMenu);
+}
+
+InstantClick.on('change', registerMenu)
