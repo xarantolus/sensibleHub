76a66ff3619c6e85e88072feb29f53ac985c8534
Philipp Erhardt <philipperhardt29@gmail.com>
2020-06-08 12:03:38 +0200

    Remove unused event functionality

--- store/event.go
+++ store/event.go
@@ -2,26 +2,6 @@ package store
 
 import "github.com/gorilla/websocket"
 
-// EventType is an event type for the Manager
-type EventType string
-
-// Event types that are possible
-const (
-	ETStartDownload    EventType = "StartDownload"
-	ETFinishedDownload EventType = "FinishedDownload"
-)
-
-// ManagerEvent is an event originating form a Manager instance
-type ManagerEvent struct {
-	Type EventType
-
-	// Progress is a number ∈ [0, 1]
-	Progress float64
-
-	// Err is a potential error. It might be non-nil if `Type` is `ETFinishedDownload`
-	Err error
-}
-
 // SetEventFunc sets the function that will be called with Manager events.
 // Used for websocket notifications
 func (m *Manager) SetEventFunc(f func(f func(c *websocket.Conn) error)) {
