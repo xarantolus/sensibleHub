5881572edb5795ed9b0dd412b911e1440bf05ce4
Philipp Erhardt <philipperhardt29@gmail.com>
2020-06-12 08:04:30 +0200

    Allow fine-tuned searches with quotes

--- store/search.go
+++ store/search.go
@@ -3,12 +3,13 @@ package store
 import (
 	"sort"
 	"strings"
+	"unicode"
 	"xarantolus/sensibleHub/store/music"
 )
 
 // Search offers search functionality
 func (m *Manager) Search(query string) (list []music.Entry) {
-	qs := strings.Fields(strings.ToUpper(query))
+	qs := splitString(strings.ToUpper(strings.TrimSpace(query)))
 	e := m.AllEntries()
 
 	type result struct {
@@ -49,26 +50,27 @@ func (m *Manager) Search(query string) (list []music.Entry) {
 
 func score(query []string, title string, multiplier int) (out int) {
 	tu := strings.ToUpper(title)
-	tf := strings.Fields(tu)
+	tf := splitString(tu)
 	if len(tf) == 0 {
 		return
 	}
 
 	for _, q := range query {
 		if strings.HasPrefix(tu, q) {
+			out += 15
+		} else {
+			out -= 2
+		}
+		if strings.Contains(tu, q) {
 			out += 10
+		} else {
+			out--
 		}
 		if contains(tf, q) {
-			out += 3
-			continue
+			out += 5
+		} else {
+			out--
 		}
-
-		if strings.Contains(tu, q) {
-			out += 2
-			continue
-		}
-
-		out -= 2
 	}
 
 	out *= multiplier
@@ -89,4 +91,32 @@ func doublePrefix(s1, s2 string) bool {
 		return true
 	}
 	return strings.HasPrefix(s2, s1)
+}
+
+// splitString splits a string at its spaces
+// Original version / source: https://play.golang.org/p/ztqfYiPSlv / https://groups.google.com/d/msg/golang-nuts/pNwqLyfl2co/APaZSSvQUAAJ
+func splitString(s string) (words []string) {
+	lastQuote := rune(0)
+	f := func(c rune) bool {
+		switch {
+		case c == lastQuote:
+			lastQuote = rune(0)
+			return false
+		case lastQuote != rune(0):
+			return false
+		case unicode.In(c, unicode.Quotation_Mark):
+			lastQuote = c
+			return false
+		default:
+			return unicode.IsSpace(c)
+		}
+	}
+
+	// Now we loop over single words and remove quotes
+	for _, word := range strings.FieldsFunc(s, f) {
+		words = append(words, strings.TrimFunc(word, func(c rune) bool {
+			return unicode.IsSpace(c) || unicode.In(c, unicode.Quotation_Mark)
+		}))
+	}
+	return
 }
