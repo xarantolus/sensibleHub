9a42d65e33f895fad5629b32b8a65ea437f144fc
xarantolus <xarantolus@protonmail.com>
2020-11-09 19:05:01 +0100

    Release: generate changelog

--- release.sh
+++ release.sh
@@ -10,3 +10,6 @@ GOOS=windows ./pack.sh "releases/sensibleHub-windows-$GIT_COMMIT.zip"
 GOOS=linux ./pack.sh "releases/sensibleHub-linux-$GIT_COMMIT.zip"
 
 GOOS=linux GOARCH=arm GOARM=5 ./pack.sh "releases/sensibleHub-raspberrypi-$GIT_COMMIT.zip"
+
+# Generate Changelog
+git log $(git describe --tags --abbrev=0)..HEAD --oneline | cut -d" " -f2- - | awk '$0="* "$0'  > release.md
