3a1914987aa4e510e5990d3480ae7f4451c3f6c3
xarantolus <xarantolus@protonmail.com>
2021-09-05 15:32:25 +0200

    Listing API

--- web/api.go
+++ web/api.go
@@ -5,24 +5,27 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"xarantolus/sensibleHub/store"
+
+	"github.com/gorilla/mux"
 )
-
-type apiSearchResult struct {
-	Query string `json:"query"`
-
-	Results []shortResult `json:"results"`
-}
-
-type shortResult struct {
-	Title string `json:"title"`
-	ID    string `json:"id"`
-}
 
 // HandleAPISongSearch is for the search API
 func (s *server) HandleAPISongSearch(w http.ResponseWriter, r *http.Request) (err error) {
+	type shortResult struct {
+		Title string `json:"title"`
+		ID    string `json:"id"`
+	}
+
+	type apiSearchResult struct {
+		Query string `json:"query"`
+
+		Results []shortResult `json:"results"`
+	}
+
 	query := r.URL.Query().Get("q")
 	if query == "" {
-		return fmt.Errorf("Empty query")
+		return fmt.Errorf("empty query")
 	}
 
 	limit := 5
@@ -47,4 +50,31 @@ func (s *server) HandleAPISongSearch(w http.ResponseWriter, r *http.Request) (er
 
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(searchResult)
+}
+
+// HandleAPIListing shows an API listing given via an mux URL variable
+func (s *server) HandleAPIListing(w http.ResponseWriter, r *http.Request) (err error) {
+	var possibleListings = map[string]func() []store.Group{
+		"title":          s.m.GroupByTitle,
+		"artist":         s.m.GroupByArtist,
+		"year":           s.m.GroupByYear,
+		"incomplete":     s.m.Incomplete,
+		"unsynced":       s.m.Unsynced,
+		"recentlyedited": s.m.RecentlyEdited,
+	}
+
+	vars := mux.Vars(r)
+	if vars == nil {
+		return fmt.Errorf("expected listing type in URL")
+	}
+
+	listingType := vars["listing"]
+
+	listFunc, ok := possibleListings[listingType]
+	if !ok {
+		return fmt.Errorf("invalid listing type %q", listingType)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(listFunc())
 }
